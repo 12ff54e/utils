c6fc53219f0796c84849a75f70dfcd766367a7c8
qzhong <12ff54e@gmail.com>
2025-03-03 09:50:11 +0800

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -81,14 +81,15 @@ void Timer::print() {
         max_length = max_length < name.size() ? name.size() : max_length;
     }
 
-    std::cout << '+';
-    for (std::size_t i = 0; i < max_length + 16; ++i) { std::cout << '-'; }
+    std::cout << '+' << std::setfill('-') << std::setw(max_length + 16) << '-'
+              << std::setfill(' ');
     std::cout << "+\n"
               << std::left << std::setw(static_cast<int>(max_length + 17))
               << "| Time consumption";
-    std::cout << "|\n+";
-    for (std::size_t i = 0; i < max_length; ++i) { std::cout << "-"; }
-    std::cout << "--+-------------+\n";
+    std::cout << "|\n";
+    std::cout << '+' << std::setfill('-') << std::setw(max_length + 2) << '-'
+              << std::setfill(' ');
+    std::cout << "+-------------+\n";
     for (const auto& name : entries) {
         auto& time_used = time_consuming.at(name).first;
         std::cout << std::left << "| "
@@ -97,9 +98,9 @@ void Timer::print() {
                   << duration<double, milliseconds::period>(time_used).count()
                   << "ms |\n";
     }
-    std::cout << '+';
-    for (std::size_t i = 0; i < max_length; ++i) { std::cout << "-"; }
-    std::cout << "--+-------------+\n";
+    std::cout << '+' << std::setfill('-') << std::setw(max_length + 2) << '-'
+              << std::setfill(' ');
+    std::cout << "+-------------+\n";
 }
 
 Timer& Timer::get_timer() {
@@ -107,5 +108,5 @@ Timer& Timer::get_timer() {
     return timer;
 }
 
-#endif // ZQ_TIMER_IMPLEMENTATION
+#endif  // ZQ_TIMER_IMPLEMENTATION
 #endif  // ZQ_TIMER
